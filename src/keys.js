d9dfbead4edd19f5a927ad5e94d8127f737314e4
devconstruct <130005343+devconstruct@users.noreply.github.com>
2023-07-10 09:12:20 -0600

    Update keys.js

--- src/keys.js
+++ src/keys.js
@@ -1,12 +1,37 @@
+//module.exports = {
+//  database:{
+//    host: 'sql.freedb.tech',
+//    port: '3306',
+//    user: 'freedb_ucy0srrzvb1jeo8o',
+//    password: 'BwMeJ8FK@Y7dgN8',
+//    database: 'freedb_but56ccwtno412ncbajp',
+//  }
+//};
+
+//Conexion buena
 module.exports = {
   database: {
-    host: 'sql.freedb.tech',
+    host: 'but56ccwtno412ncbajp-mysql.services.clever-cloud.com',
     port: '3306',
-    user: 'freedb_ucy0srrzvb1jeo8o',
-    password: 'BwMeJ8FK@Y7dgN8',
-    database: 'freedb_but56ccwtno412ncbajp'
+    user: 'ucy0srrzvb1jeo8o',
+    password: 'RIRZ7gnHKPpJj1p2Z5YM',
+    database: 'but56ccwtno412ncbajp'
   }
 };
+
+//---------------------------------------
+//module.exports = {
+//  database: {
+//    host: 'containers-us-west-5.railway.app',
+//    port: '7041',
+//    user: 'root',
+//    password: 'nYfhjprl5YmS1BBJM2G6',
+//    database: 'railway'
+//  }
+//};
+
+//----------------------------------
+
 // module.exports = {
 //   database: {
 //     host: 'localhost',
